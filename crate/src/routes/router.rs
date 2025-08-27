e4b89a6efb9603a87dd95baa61cf9370f16a2c99
Sui Gn <natsynapses@gmail.com>
2025-08-27 12:14:40 +0200

    running stable

--- crate/src/routes/router.rs
+++ crate/src/routes/router.rs
@@ -3,10 +3,14 @@ use actix_web::web::{self, ServiceConfig};
 use actix_web::guard;
 use crate::routes::{rest, graphql};
 use crate::routes::ws::me_ws::me_ws_handler; 
-
 pub fn config(cfg: &mut ServiceConfig) {
     // Crea el AppState e inyecta al schema
     let schema = web::Data::new(graphql::create_schema());
+    // --- Root Route ---
+    cfg.service(
+        web::resource("/")
+            .route(web::get().to(|| async { "monad.ai on" }))
+    );
 
     // --- GraphQL Routes --- 
     cfg.service(
@@ -26,10 +30,8 @@ pub fn config(cfg: &mut ServiceConfig) {
     // --- REST Routes ---
     cfg.service(rest::status);
     cfg.service(rest::request_history_logs);
-
     // --- this.me Routes ---
     cfg.service(rest::list_us);
-
     // --- WebSocket Routes ---
     cfg.service(
         web::resource("/ws/me").route(web::get().to(me_ws_handler)) // ✅ Properly register WebSocket
